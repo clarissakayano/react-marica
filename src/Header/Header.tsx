1c2f05a2cd91f9205cf43c922b10196df994c9a3
kay <clarissa_kayano@hotmail.com>
2022-09-13 16:15:16 -0300

    testando

---
+++ src/Header/Header.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react'
+
+const Header: React.FC = () => (
+  <div>
+    <p>Teste</p>
+  </div>
+)
+export default memo(Header)
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import 'services/i18n'
 
