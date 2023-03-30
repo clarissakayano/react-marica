9411e2fe17452cee46246fcc1d7604d93d8ce003
kay <clarissa_kayano@hotmail.com>
2023-03-29 22:55:22 -0300

    Corrigindo

--- src/pages/SpaceCategories/SpaceCategories.tsx
+++ src/pages/SpaceCategories/SpaceCategories.tsx
@@ -45,7 +45,7 @@ const SpaceCategories: React.FC = () => {
   const titleNomalized = categories.find((space) => space.id === Number(id))
 
   useEffect(() => {
-    setTitle('PontosTuristicos')
+    setTitle('Espaços Para Eventos')
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [i18n.resolvedLanguage])
 
@@ -97,20 +97,17 @@ const SpaceCategories: React.FC = () => {
                 </div>
               </Col>
 
-              <div className="d-flex flex-wrap mb-4 mt-2 g-3">
-                <div className="d-flex flex-wrap mb-4 mt-2 g-3">
-                  {!isLoading &&
-                    spaces.map((space) => (
-                      <Row key={space.id} className="flex-nowrap flex-md-wrap">
-                        <Col className="col-md-6 col-lg-4 mb-3 mb-md-5">
-                          <Link to={`/espacos/${space.id}`}>
-                            <GeralCard item={space} />
-                          </Link>
-                        </Col>
-                      </Row>
+              {!isLoading && (
+                <div className="pd">
+                  <Row className="justify-content-start row-cols-1 row-cols-md-2 row-cols-lg-3">
+                    {spaces.map((space) => (
+                      <Col className="d-flex mb-3 mb-md-5 px-1" key={space.id}>
+                        <GeralCard item={space} pagelink="espaco" />
+                      </Col>
                     ))}
+                  </Row>
                 </div>
-              </div>
+              )}
               {!isLoading && !error && spaces.length === 0 && (
                 <div className="d-flex justify-content-center">
                   <h2>Nenhum resultado encontrado</h2>
